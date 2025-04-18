fbff54647fb9fc7f80f3fa6d390709a514d1b2c0
NikithaJain-git <nikithajain56789@gmail.com>
2025-04-18 19:17:04 +0530

    Update install_tools.sh

--- terraform/install_tools.sh
+++ terraform/install_tools.sh
@@ -1,44 +1,84 @@
 #!/bin/bash
 
-# Update system and install core packages
-sudo apt update
-sudo apt install -y fontconfig openjdk-17-jre 
+#Docker Installation
+sudo apt-get update
+sudo apt-get install docker.io
+sudo usermod -aG docker $USER
+newgrp docker
+docker --version
 
-# Jenkins installation
+#aws cli Installation
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+unzip awscliv2.zip
+sudo ./aws/install
+
+#kubectl Installation
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version
+
+#Java installation
+sudo apt update -y
+sudo apt install fontconfig openjdk-17-jre -y
+
+#Jenkins installation
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get -y install jenkins
+
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+jenkins --version
 
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
 
-# Docker installation
-sudo apt-get update
-sudo apt-get install docker.io -y
-
-# User group permission
-sudo usermod -aG docker $USER
-sudo usermod -aG docker jenkins
-
-sudo systemctl restart docker
-sudo systemctl restart jenkins
-
-# Install dependencies and Trivy
-sudo apt-get install wget apt-transport-https gnupg lsb-release snapd -y
+#Trivy Installation
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update -y
 sudo apt-get install trivy -y
+trivy version
 
-# AWS CLI installation
-sudo snap install aws-cli --classic
+#Helm Installation
+$ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+$ chmod 700 get_helm.sh
+$ ./get_helm.sh
+helm version
 
-# Helm installation
-sudo snap install helm --classic
+#To install ingress-controller with helm
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
 
-# Kubectl installation
-sudo snap install kubectl --classic
+#Argocd installation using helm
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+kubectl create namespace argocd
+helm install argocd argo/argo-cd -n argocd
+
+#Install Argocd CLI
+curl -sSL -o argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+chmod +x argocd
+sudo mv argocd /usr/local/bin/
+
+#changing the svc to Nodeport
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
+kubectl get svc -n argocd
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+
+#Install prometheus and grafana using Helm
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+kubectl create namespace prometheus
+kubectl get ns
+helm install stable prometheus-community/kube-prometheus-stack -n prometheus
+kubectl get pods -n prometheus
+kubectl get svc -n prometheus
+kubectl patch svc stable-grafana -n prometheus -p '{"spec": {"type":"NodePort"}}'
+
+
